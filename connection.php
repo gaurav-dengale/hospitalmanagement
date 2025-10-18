3ca1f13e8122ff8a1a305fd3ec22f4d245cb4038
Gaurav Dengale <151223338+gaurav-dengale@users.noreply.github.com>
2025-10-18 14:55:14 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "db";
+$username = "root";  
+$password = "root"; 
+$dbname = "edoc"; 
+
+$database = new mysqli($servername, $username, $password, $dbname);
+
+if ($database->connect_error) {
+    die("Échec de la connexion : " . $database->connect_error);
+}
+?>
+
